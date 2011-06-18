5c3116d9b098027725b2425a53fa1b86e768a6a8
pallenpost <pallenpost@cbe1c1a3-2f18-6bcf-d57f-239f278cdb42>
2011-06-18 00:25:30 +0000

    Changed indent style to 4 spaces. Added license. Small tweak with tests.

--- python/ip4addr.py
+++ python/ip4addr.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 #
-# IPv4 address class
-# This uses inet_pton and inet_ntop, which may not be available in
-# Windows cpython
+# IP version 4 address class
+# Copyright (C) 2011  Patrick Allen
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
+
+# This uses inet_pton, ntohl, etc, which may or may not be available in
+# Windows cpython.
 #
 # Addresses and mask are set either using a string in dotted decimal format,
 # a host order integer, or (for masks) an integer prefix length.
@@ -23,253 +40,249 @@ from _struct import pack
 from socket import ntohl, htonl, inet_ntop, inet_pton, AF_INET, error
 
 class IP4Addr:
-	def getAddr(self):
-		if (self.isValid() == False):
-			return None, None
+    def getAddr(self):
+        if (self.isValid() == False):
+            return None, None
 
-		#hboaddr = ntohl(self._addr_i)
+        #hboaddr = ntohl(self._addr_i)
 
-		return self._addr_s, self._addr_i
+        return self._addr_s, self._addr_i
 
-	def getMask(self, buf = None):
-		if (self.isValid() == False):
-			return None, None
+    def getMask(self, buf = None):
+        if (self.isValid() == False):
+            return None, None
 
-		return self._mask_s, self._mask_i
+        return self._mask_s, self._mask_i
 
-	def getBroadcast(self):
-		if (self.isValid() == False):
-			return None, None
+    def getBroadcast(self):
+        if (self.isValid() == False):
+            return None, None
 
-		bc_i = self._addr_i | self.getHostmask()[1]
-		bc_s = ntohl(bc_i)
-		bc_s = inet_ntop(AF_INET, pack("L", bc_s))
+        bc_i = self._addr_i | self.getHostmask()[1]
+        bc_s = ntohl(bc_i)
+        bc_s = inet_ntop(AF_INET, pack("L", bc_s))
 
-		return bc_s, bc_i
+        return bc_s, bc_i
 
-	def getHostmask(self):
-		if (self.isValid() == False):
-			return None, None
+    def getHostmask(self):
+        if (self.isValid() == False):
+            return None, None
 
-		# AND with 0xFFFFFFFF in case of different length integers
-		hmask_i = ~ self.getMask()[1] & 0xFFFFFFFF
-		hmask_s = ntohl(hmask_i)
-		hmask_s = inet_ntop(AF_INET, pack("L", hmask_s))
+        # AND with 0xFFFFFFFF in case of different length integers
+        hmask_i = ~ self.getMask()[1] & 0xFFFFFFFF
+        hmask_s = ntohl(hmask_i)
+        hmask_s = inet_ntop(AF_INET, pack("L", hmask_s))
 
-		return hmask_s, hmask_i
+        return hmask_s, hmask_i
 
-	def getNetwork(self):
-		if (self.isValid() == False):
-			return None, None
+    def getNetwork(self):
+        if (self.isValid() == False):
+            return None, None
 
-		net_i = self._mask_i & self._addr_i
-		net_s = ntohl(net_i)
-		net_s = inet_ntop(AF_INET, pack("L", net_s))
+        net_i = self._mask_i & self._addr_i
+        net_s = ntohl(net_i)
+        net_s = inet_ntop(AF_INET, pack("L", net_s))
 
-		return net_s, net_i
+        return net_s, net_i
 
-	def getPlen(self):
-		if (self.isValid() == False):
-			return None
+    def getPlen(self):
+        if (self.isValid() == False):
+            return None
 
-		return self._plen
+        return self._plen
 
-	@staticmethod
-	def add_octets(addrstr):
-		addrbytes = bytearray(addrstr)
-		return (addrbytes[0] << 24) + \
-		       (addrbytes[1] << 16) + \
-		       (addrbytes[2] << 8)  + \
-		       (addrbytes[3])
+    @staticmethod
+    def add_octets(addrstr):
+        addrbytes = bytearray(addrstr)
+        return (addrbytes[0] << 24) + \
+               (addrbytes[1] << 16) + \
+               (addrbytes[2] << 8)  + \
+               (addrbytes[3])
 
-	# Mask may be either a subnet mask (in dotted decimal form as a string,
-	# or a packed 32 bit address), or a prefix length.
-	#
-	# We'll determine if it's a mask or prefix length by attempting to use
-	# it as a subnet mask first; if that fails, we'll try using it as a
-	# prefix length.
-	#
-	# So, the following are all valid and equivalent:
-	#    IP4Addr("192.168.0.1", 24)
-	#    IP4Addr("192.168.0.1", "255.255.255.0")
-	#    IP4Addr(16820416, 16777215)   # Host order integers
-	#
-	# If no mask is given, a 32 bit mask is assumed
-	#    IP4Addr("192.168.0.1") -> Assumes 32 bit mask
-	#    IP4Addr(16820416) ->  # Host order integer with assumed 32 bit mask
-	# 
-	def __init__(self, addr, mask = 32):
-		self.setAddr(addr)
-		self.setMask(mask)
+    # Mask may be either a subnet mask (in dotted decimal form as a string,
+    # or a packed 32 bit address), or a prefix length.
+    #
+    # We'll determine if it's a mask or prefix length by attempting to use
+    # it as a subnet mask first; if that fails, we'll try using it as a
+    # prefix length.
+    #
+    # So, the following are all valid and equivalent:
+    #    IP4Addr("192.168.0.1", 24)
+    #    IP4Addr("192.168.0.1", "255.255.255.0")
+    #    IP4Addr(16820416, 16777215)   # Host order integers
+    #
+    # If no mask is given, a 32 bit mask is assumed
+    #    IP4Addr("192.168.0.1") -> Assumes 32 bit mask
+    #    IP4Addr(16820416) ->  # Host order integer with assumed 32 bit mask
+    # 
+    def __init__(self, addr, mask = 32):
+        self.setAddr(addr)
+        self.setMask(mask)
 
-	def setMask(self, mask):
-		if (isinstance(mask, int) or isinstance(mask, long)):
-			if ((IP4Addr.isValidPlen(mask)) == True):
-				self._plen = mask
-				mask_hostorder = IP4Addr.pltosm(self._plen)
-				self._mask_s = inet_ntop(AF_INET,
-				               pack("L", mask_hostorder))
-				self._mask_i = htonl(mask_hostorder)
-				return self._setMaskSuccess()
-			else:
-				try:
-					self._mask_s = inet_ntop(AF_INET,
-					                pack("L", mask))
-					self._mask_i = htonl(mask)
-					self._plen = IP4Addr.smtopl(
-					                           self._mask_i)
-					return self._setMaskSuccess()
-				except error:
-					return self._setMaskFail()
-		elif (isinstance(mask, str)):
-			try:
-				maskbytes = inet_pton(AF_INET, mask)
-				self._mask_i = IP4Addr.add_octets(maskbytes)
-			 	self._mask_s = mask
-			 	self._plen = IP4Addr.smtopl(mask)
-			except error:
-				return self._setMaskFail()
-		else:
-			# Bad type, should throw an exception
-			return self._setMaskFail()
+    def setMask(self, mask):
+        if (isinstance(mask, int) or isinstance(mask, long)):
+            if ((IP4Addr.isValidPlen(mask)) == True):
+                self._plen = mask
+                mask_hostorder = IP4Addr.pltosm(self._plen)
+                self._mask_s = inet_ntop(AF_INET, pack("L", mask_hostorder))
+                self._mask_i = htonl(mask_hostorder)
+                return self._setMaskSuccess()
+            else:
+                try:
+                    self._mask_s = inet_ntop(AF_INET, pack("L", mask))
+                    self._mask_i = htonl(mask)
+                    self._plen = IP4Addr.smtopl(self._mask_i)
+                    return self._setMaskSuccess()
+                except error:
+                    return self._setMaskFail()
+        elif (isinstance(mask, str)):
+            try:
+                maskbytes = inet_pton(AF_INET, mask)
+                self._mask_i = IP4Addr.add_octets(maskbytes)
+                self._mask_s = mask
+                self._plen = IP4Addr.smtopl(mask)
+            except error:
+                return self._setMaskFail()
+        else:
+            # Bad type, should throw an exception
+            return self._setMaskFail()
 
-		return self._setMaskSuccess()
+        return self._setMaskSuccess()
 
-	# Subnet mask to prefix length
-	@staticmethod
-	def smtopl(mask):
-		plen = 0
+    # Subnet mask to prefix length
+    @staticmethod
+    def smtopl(mask):
+        plen = 0
 
-		if ((isinstance(mask, int) != True) and
-		    (isinstance(mask, long) != True)):
-			return None
+        if ((isinstance(mask, int) != True) and
+            (isinstance(mask, long) != True)):
+            return None
 
-		# Load the value of each byte into a list of bytes
-		maskbytes = []
-		maskbytes.append((mask & 0xFF000000) >> 24)
-		maskbytes.append((mask & 0x00FF0000) >> 16)
-		maskbytes.append((mask & 0x0000FF00) >> 8 )
-		maskbytes.append(mask & 0x000000FF)
+        # Load the value of each byte into a list of bytes
+        maskbytes = []
+        maskbytes.append((mask & 0xFF000000) >> 24)
+        maskbytes.append((mask & 0x00FF0000) >> 16)
+        maskbytes.append((mask & 0x0000FF00) >> 8 )
+        maskbytes.append(mask & 0x000000FF)
 
-		for maskbyte in maskbytes:
-			if (maskbyte == 255):
-				# All bits were on
-				plen += 8
-			else:
-				# Find which bits are on in this octet
-				bits = [ 128, 64, 32, 16, 8, 4, 2, 1 ]
+        for maskbyte in maskbytes:
+            if (maskbyte == 255):
+                # All bits were on
+                plen += 8
+            else:
+                # Find which bits are on in this octet
+                bits = [ 128, 64, 32, 16, 8, 4, 2, 1 ]
 
-				for bit in bits:
-					if (bit & maskbyte == 0):
-						break
-					plen += 1
+                for bit in bits:
+                    if (bit & maskbyte == 0):
+                        break
+                    plen += 1
 
-		return plen
+        return plen
 
-	# Prefix length to subnet mask in host order
-	@staticmethod
-	def pltosm(plen):
-		if (not IP4Addr.isValidPlen(plen)):
-			return None
+    # Prefix length to subnet mask in host order
+    @staticmethod
+    def pltosm(plen):
+        if (not IP4Addr.isValidPlen(plen)):
+            return None
 
-		if (plen == 0):
-			return 0
+        if (plen == 0):
+            return 0
 
-		if (plen > 32):
-			plen = 32
+        if (plen > 32):
+            plen = 32
 
-		highbit = 1 << 31    # Highest bit flipped on
+        highbit = 1 << 31    # Highest bit flipped on
 
-		# 1.  Begin with a mask of the highest bit flipped on
-		# 2.  Right shift mask by one less than the prefix length
-		# 3.  Subtract 1 from mask; now all bits we want are 0
-		# 4.  Get the one's complement of mask
-		# 4a. AND with 0xFFFFFFFF to counter python's type system
-		#     (will appear as a negative number otherwise)
-		# 5.  Mask is in network byte order, convert to host order
-		return ntohl(~((highbit >> plen - 1) - 1) & 0xFFFFFFFF)
+        # 1.  Begin with a mask of the highest bit flipped on
+        # 2.  Right shift mask by one less than the prefix length
+        # 3.  Subtract 1 from mask; now all bits we want are 0
+        # 4.  Get the one's complement of mask
+        # 4a. AND with 0xFFFFFFFF to counter python's type system
+        #     (will appear as a negative number otherwise)
+        # 5.  Mask is in network byte order, convert to host order
+        return ntohl(~((highbit >> plen - 1) - 1) & 0xFFFFFFFF)
 
-	def isValidMask(mask):
-		if (not isinstance(mask, int)):
-			return False
+    def isValidMask(mask):
+        if (not isinstance(mask, int)):
+            return False
 
-		bits = [ 128, 64, 32, 16, 8, 4, 2, 1 ]
-		hostbits = False
+        bits = [ 128, 64, 32, 16, 8, 4, 2, 1 ]
+        hostbits = False
 
-		# For every octet, check if mask is ok
-		for maskbyte in bytearray(mask):
-			for bit in bits:
-				if (hostbits == False):
-					if (maskbyte & bit):
-						continue
-					else:
-						hostbits = True
-				else:
-					if (maskbyte & bit):
-						# Bad mask
-						return False
-					else:
-						continue
+        # For every octet, check if mask is ok
+        for maskbyte in bytearray(mask):
+            for bit in bits:
+                if (hostbits == False):
+                    if (maskbyte & bit):
+                        continue
+                    else:
+                        hostbits = True
+                else:
+                    if (maskbyte & bit):
+                        # Bad mask
+                        return False
+                    else:
+                        continue
 
-		return True
+        return True
 
-	@staticmethod
-	def isValidPlen(plen):
-		if (not isinstance(plen, int)):
-			return False
+    @staticmethod
+    def isValidPlen(plen):
+        if (not isinstance(plen, int)):
+            return False
 
-		if (plen > 32 or plen < 0):
-			return False
+        if (plen > 32 or plen < 0):
+            return False
 
-		return True
+        return True
 
-	def setAddr(self, addr):
-		if (isinstance(addr, int)):
-			try:
-				self._addr_s = inet_ntop(AF_INET,
-				                                pack("L", addr))
-				self._addr_i = htonl(addr)
-				return self._setAddrSuccess()
-			except error:
-				return self._setAddrFail()
-		elif (isinstance(addr, str)):
-			try:
-				#addrbytes = inet_pton(AF_INET, addr)
-				#self._addr_i = self.add_octets(addrbytes)
-				self._addr_s = addr
-				self._addr_i = inet_pton(AF_INET, self._addr_s)
-				self._addr_i = IP4Addr.add_octets(self._addr_i)
+    def setAddr(self, addr):
+        if (isinstance(addr, int)):
+            try:
+                self._addr_s = inet_ntop(AF_INET, pack("L", addr))
+                self._addr_i = htonl(addr)
+                return self._setAddrSuccess()
+            except error:
+                return self._setAddrFail()
+        elif (isinstance(addr, str)):
+            try:
+                #addrbytes = inet_pton(AF_INET, addr)
+                #self._addr_i = self.add_octets(addrbytes)
+                self._addr_s = addr
+                self._addr_i = inet_pton(AF_INET, self._addr_s)
+                self._addr_i = IP4Addr.add_octets(self._addr_i)
 
-				return self._setAddrSuccess()
-			except error:
-				return self._setAddrFail()
-		else:
-			# Bad type, should throw an exception
-			return self._setAddrFail()
+                return self._setAddrSuccess()
+            except error:
+                return self._setAddrFail()
+        else:
+            # Bad type, should throw an exception
+            return self._setAddrFail()
 
-	def _setAddrSuccess(self):
-		self._addr_valid = True
-		return True
+    def _setAddrSuccess(self):
+        self._addr_valid = True
+        return True
 
-	def _setAddrFail(self):
-		self._addr_valid = False
-		return False
+    def _setAddrFail(self):
+        self._addr_valid = False
+        return False
 
-	def _setMaskSuccess(self):
-		self._mask_valid = True
-		return True
+    def _setMaskSuccess(self):
+        self._mask_valid = True
+        return True
 
-	def _setMaskFail(self):
-		self._mask_valid = False
-		return False
+    def _setMaskFail(self):
+        self._mask_valid = False
+        return False
 
-	def isValid(self):
-		return self._addr_valid and self._mask_valid
+    def isValid(self):
+        return self._addr_valid and self._mask_valid
 
-	def printAll(self):
-		print "Mask:", addr.getMask()
-		print "Addr:", addr.getAddr()
-		print "Bcast:", addr.getBroadcast()
-		print "Hmask:", addr.getHostmask()
-		print "Net:", addr.getNetwork()
-		print "Plen:", addr.getPlen()
+    def printAll(self):
+        print "Mask:", addr.getMask()
+        print "Addr:", addr.getAddr()
+        print "Bcast:", addr.getBroadcast()
+        print "Hmask:", addr.getHostmask()
+        print "Net:", addr.getNetwork()
+        print "Plen:", addr.getPlen()
--- python/ip4addrtests.py
+++ python/ip4addrtests.py
@@ -1,52 +1,68 @@
 #!/usr/bin/env python
+# Unit tests.
+# To add a new test, write a function beginning with "ip4addr_test".
 
 from ip4addr import IP4Addr
 import sys
 
 def assert_equals(res, good):
-	if (res != good):
-		print "Failed.\n***\nDetails:\nResult was: ", res, "\n", \
-		      "Should have been:", good
-		print "***"
-		raise AssertionError
-	else:
-		return
+    if (res != good):
+        raise AssertionError("***\n"
+                             " Details:\n"
+                             " Result was:", res, "\n",
+                             " Should have been:", good, "\n",
+                             "***")
+    else:
+        return
+
+def sanity():
+    assert_equals(1, 1)
+
+    return
 
 def ip4addr_test_good_str_int24():
-	addr = IP4Addr("192.168.1.1", 16777215)
+    addr = IP4Addr("192.168.1.1", 16777215)
 
-	assert_equals(addr.isValid(), True)
-	assert_equals(addr.getAddr(), ("192.168.1.1", 3232235777))
-	assert_equals(addr.getMask(), ("255.255.255.0", 4294967040))
-	assert_equals(addr.getHostmask(), ("0.0.0.255", 255))
-	assert_equals(addr.getBroadcast(), ("192.168.1.255", 3232236031))
-	assert_equals(addr.getNetwork(), ("192.168.1.0", 3232235776))
-	assert_equals(addr.getPlen(), 24)
+    assert_equals(addr.isValid(), True)
+    assert_equals(addr.getAddr(), ("192.168.1.1", 3232235777))
+    assert_equals(addr.getMask(), ("255.255.255.0", 4294967040))
+    assert_equals(addr.getHostmask(), ("0.0.0.255", 255))
+    assert_equals(addr.getBroadcast(), ("192.168.1.255", 3232236031))
+    assert_equals(addr.getNetwork(), ("192.168.1.0", 3232235776))
+    assert_equals(addr.getPlen(), 24)
 
-	return
+    return
 
 def ip4addr_test_good_str_str24():
-	addr = IP4Addr("192.168.1.1", "255.255.255.0")
+    addr = IP4Addr("192.168.1.1", "255.255.255.0")
 
-	assert_equals(addr.isValid(), True)
-	assert_equals(addr.getAddr(), ("192.168.1.1", 3232235777))
-	assert_equals(addr.getMask(), ("255.255.255.0", 4294967040))
-	assert_equals(addr.getHostmask(), ("0.0.0.255", 255))
-	assert_equals(addr.getBroadcast(), ("192.168.1.255", 3232236031))
-	assert_equals(addr.getNetwork(), ("192.168.1.0", 3232235776))
-	assert_equals(addr.getPlen(), 24)
+    assert_equals(addr.isValid(), True)
+    assert_equals(addr.getAddr(), ("192.168.1.1", 3232235777))
+    assert_equals(addr.getMask(), ("255.255.255.0", 4294967040))
+    assert_equals(addr.getHostmask(), ("0.0.0.255", 255))
+    assert_equals(addr.getBroadcast(), ("192.168.1.255", 3232236031))
+    assert_equals(addr.getNetwork(), ("192.168.1.0", 3232235776))
+    assert_equals(addr.getPlen(), 24)
 
-	return
-
-def ip4addr_test_another():
-	assert True
+    return
 
 if (__name__ == "__main__"):
-	# Find all methods starting with "ip4addr_test" and run them.
-	for test in dir(sys.modules[__name__]):
-		if (test.startswith("ip4addr_test")):
-			sys.stdout.write("Running " + test + "... ")
-			getattr(sys.modules[__name__], test)()
-			sys.stdout.write("OK\n")
+    try:
+        print "Running sanity check...",
+        sanity()
+        print "Passed"
 
-	print "All tests succeeded."
+        # Find all methods starting with "ip4addr_test" and run them.
+        for test in dir(sys.modules[__name__]):
+            if (test.startswith("ip4addr_test")):
+                sys.stdout.write("Running " + test + "... ")
+                getattr(sys.modules[__name__], test)()
+                sys.stdout.write("OK\n")
+    except AssertionError as e:
+        print "Failed"
+        for arg in e.args:
+            print arg,
+        e.args = ""
+        raise
+
+    print "All tests succeeded."
